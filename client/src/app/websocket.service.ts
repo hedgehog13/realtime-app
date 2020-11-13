20648742e9f2e3b63d75d1bc0072287f6e563191
hedgehog13 <ira.varshavsky@gmail.com>
2020-11-13 08:36:25 -0500

    'test'

--- client/src/app/websocket.service.ts
+++ client/src/app/websocket.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import * as io from 'socket.io-client';
+
 import { Observable } from 'rxjs';
 import * as Rx from 'rxjs/Rx';
 import { environment } from '../environments/environment';
@@ -12,26 +12,26 @@ export class WebsocketService {
 
   constructor() { }
 
-  connect(): Subject<MessageEvent> {
-    this.socket = io.io('wss://pubsub-edge.twitch.tv');
-
-    let observable = new Observable(observer => {
-      this.socket.on('message', (data) => {
-        console.log("Received message from Websocket Server");
-        observer.next(data);
-      })
-      return () => {
-        this.socket.disconnect();
-      }
-    });
-    let observer = {
-      next: (data: Object) => {
-        this.socket.emit('message', JSON.stringify(data));
-      },
-    };
-
-    // we return our Rx.Subject which is a combination
-    // of both an observer and observable.
-    return Subject.create(observer, observable);
-  }
+  // connect(): Subject<MessageEvent> {
+  //   this.socket = io.io('wss://pubsub-edge.twitch.tv');
+  //
+  //   let observable = new Observable(observer => {
+  //     this.socket.on('message', (data) => {
+  //       console.log("Received message from Websocket Server");
+  //       observer.next(data);
+  //     })
+  //     return () => {
+  //       this.socket.disconnect();
+  //     }
+  //   });
+  //   let observer = {
+  //     next: (data: Object) => {
+  //       this.socket.emit('message', JSON.stringify(data));
+  //     },
+  //   };
+  //
+  //   // we return our Rx.Subject which is a combination
+  //   // of both an observer and observable.
+  //   return Subject.create(observer, observable);
+ // }
 }
